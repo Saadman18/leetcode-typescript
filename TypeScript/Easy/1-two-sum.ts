f7fadb46a56f1e438663429767310d7447ace394
Saadman Sakib <saadman.sakib.ss@gmail.com>
2022-08-12 01:21:09 +0600

    two sum improved

--- TypeScript/Easy/1-two-sum.ts
+++ TypeScript/Easy/1-two-sum.ts
@@ -1,13 +1,28 @@
-function twoSum(nums: number[], target: number): number[] {
-    for(let i=0;i<nums.length;i++){
-        for(let j=i+1;j<nums.length;j++){
-            if(nums[i]+nums[j]==target){
-                return[i,j]; 
-            }
+// function twoSum(nums: number[], target: number): number[] {
+//     for(let i=0;i<nums.length;i++){
+//         for(let j=i+1;j<nums.length;j++){
+//             if(nums[i]+nums[j]==target){
+//                 return[i,j]; 
+//             }
             
+//         }
+//     }
+//     return [];
+
+// };
+
+
+function twoSum(nums: number[], target: number): number[] {
+    let key = {}
+    
+    for(let i=0; i<nums.length;i++){
+        
+        if(key.hasOwnProperty(target-nums[i])){
+            return [key[target-nums[i]], i]
+        }
+        else{
+            key[nums[i]] = i
         }
     }
-    return [];
 
 };
-
